b9439c53700ab37ceae37fb7530326ee6d447464
Liu Chuanshi <liuchuanshi3688@gmail.com>
2023-09-22 12:13:35 +0800

    Add swagger dependency

---
+++ src/main/java/org/springboard/tsc/configuration/SpringMvcConfig.java
@@ -0,0 +1,8 @@
+package org.springboard.tsc.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+
+@Configuration
+public class SpringMvcConfig extends DelegatingWebMvcConfiguration {
+}
---
+++ src/main/java/org/springboard/tsc/configuration/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.springboard.tsc.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@EnableOpenApi
+@Configurable
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("Normal")
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.citi.cv.umbrella.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Umbrella Swagger3 UI")
+                .description("Designed by CV")
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+}
